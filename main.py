faf20be4cef381cd7e36581cb54bdbf7f393b2f3
GonzaloSbiglio <69168091+Gonzalo-byte@users.noreply.github.com>
2021-12-28 18:27:54 -0300

    se agrego la librería que faltaba y daba el error

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 driver = webdriver.Chrome('C:/chromedriver.exe')
 lista_autos = []
-for i in range(23):
+for i in range(22):
     url = f'https://autos.mercadolibre.com.ar/0-km/_Desde_{((i*48)+1)}_PciaId_cordoba_NoIndex_True'
     driver.get(url)
     driver.implicitly_wait(10)
@@ -17,27 +17,21 @@ for i in range(23):
     all_autos = soup.find_all('li', class_ = 'ui-search-layout__item')
     for auto in all_autos:
         precio = auto.find('span', class_ = 'price-tag-fraction').text.replace('.', '')
-        año_km = auto.find('li', class_= 'ui-search-card-attributes__attribute')
+        ano_km = auto.find('li', class_= 'ui-search-card-attributes__attribute')
         try:
-            año = año_km[0].text
+            ano = ano_km[0].text
         except:
-            año = 'Nan'
+            ano = 'Nan'
         try:
-            km = año_km[1].text
+            km = ano_km[1].text
         except:
             km = 'Nan'
     modelo = auto.find('h2', class_ = 'ui-search-item__title ui-search-item__group__element').text
     locacion = auto.find('span', class_ = 'ui-search-item__group__element ui-search-item__location').text
     link = auto.find('a')['href']
-    auto_data = [precio, año, km, modelo, locacion, link]
+    auto_data = [precio, ano, km, modelo, locacion, link]
     lista_autos.append(auto_data)
 
-try:
-    df = pd.DataFrame(lista_autos, columns=['precio', 'año', 'km', 'modelo', 'ubicacion', 'link'])
-    df.to_excel('autos.xlsx')
-    print('Exportación completa')
-except:
-    print('Error al exportar')
-
+df = pd.DataFrame(data=lista_autos, columns=['precio', 'ano', 'km', 'modelo', 'locacion', 'link'])
+df.to_excel('autos.xlsx')
 driver.close()
-    
